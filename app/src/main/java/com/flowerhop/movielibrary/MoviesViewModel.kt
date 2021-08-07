bae7996fea15db7b658ab3c0d331c706b56edceb
Flowerhop <l941166@hotmail.com>
2021-08-07 23:09:13 +0800

    Show now playing list with RecyclerView.

--- app/src/main/java/com/flowerhop/movielibrary/MoviesViewModel.kt
+++ app/src/main/java/com/flowerhop/movielibrary/MoviesViewModel.kt
@@ -2,12 +2,13 @@ package com.flowerhop.movielibrary
 
 import androidx.lifecycle.*
 import com.flowerhop.movielibrary.network.entity.Movie
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MoviesViewModel(val repo: MovieRepository): ViewModel() {
     var nowPlayings = MutableLiveData<List<Movie>>().apply {
-        viewModelScope.launch {
-            value = repo.getNowPlaying(1).movies
+        viewModelScope.launch(Dispatchers.IO) {
+            postValue(repo.getNowPlaying(1).movies)
         }
     }
 }
--- app/src/test/java/com/flowerhop/movielibrary/MoviesViewModelTest.kt
+++ app/src/test/java/com/flowerhop/movielibrary/MoviesViewModelTest.kt
@@ -1,9 +1,10 @@
 package com.flowerhop.movielibrary
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.flowerhop.movielibrary.network.entity.Movie
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,21 +14,18 @@ class MoviesViewModelTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
-    @Before
-    fun setup() {
-
-    }
-
     @Test
-    fun `Can get movie list`() {
+    fun `Can notify movie list change`() {
         // Arrange
+        var called = false
         val moviesViewModel = MoviesViewModel(MovieRepository())
-
-        // Act
-        moviesViewModel.nowPlayings.observeForever {
-            // Assert
-            println("sss")
-            Assert.assertNull(moviesViewModel.nowPlayings)
+        val observer = Observer<List<Movie>>() {
+            called = true
         }
+        // Act
+        moviesViewModel.nowPlayings.observeForever(observer)
+        Thread.sleep(2000)
+        Assert.assertTrue(called)
+        moviesViewModel.nowPlayings.removeObserver(observer)
     }
 }
