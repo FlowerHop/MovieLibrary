e7912621822441b607dccecfba87cf8005407653
Flowerhop <l941166@hotmail.com>
2021-10-18 00:43:41 +0800

    Try material theme.

--- app/src/main/java/com/flowerhop/movielibrary/view/MovieViewHolder.kt
+++ app/src/main/java/com/flowerhop/movielibrary/view/MovieViewHolder.kt
@@ -9,13 +9,15 @@ import com.flowerhop.movielibrary.R
 import com.flowerhop.movielibrary.network.APIClient
 import com.flowerhop.movielibrary.network.entity.Movie
 import kotlinx.android.synthetic.main.item_movie.view.*
+import kotlinx.android.synthetic.main.item_movie.view.thumbnail
+import kotlinx.android.synthetic.main.item_movie_rounded.view.*
 
 class MovieViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
-    LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+    LayoutInflater.from(parent.context).inflate(R.layout.item_movie_rounded, parent, false)
 ) {
     fun bind(movie: Movie) {
-        itemView.title.text = movie.title
-        itemView.overview.text = movie.overview
+//        itemView.title.text = movie.title
+//        itemView.overview.text = movie.overview
         Glide.with(this.itemView).load("${APIClient.IMAGE_BASE_URL}${movie.posterPath}").into(itemView.thumbnail)
         itemView.setOnClickListener {
             MovieBottomSheetDialog.show((itemView.context as FragmentActivity).supportFragmentManager, movie)
