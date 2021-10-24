ab8f0ea586bd0e1900ab3460846210e0304f1573
Flowerhop <l941166@hotmail.com>
2021-10-24 10:02:49 +0800

    Refine the lists in HomeFragment with SimpleCategoryList.

--- app/src/main/java/com/flowerhop/movielibrary/view/HomeFragment.kt
+++ app/src/main/java/com/flowerhop/movielibrary/view/HomeFragment.kt
@@ -3,12 +3,13 @@ package com.flowerhop.movielibrary.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.flowerhop.movielibrary.R
 import com.flowerhop.movielibrary.di.Providers
+import com.flowerhop.movielibrary.domain.model.MovieCategory
 import com.flowerhop.movielibrary.presentation.MoviesAdapter
 import com.flowerhop.movielibrary.presentation.MoviesViewModel
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.simple_category_list.view.*
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
     companion object {
@@ -33,46 +34,59 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
         initNowPlaying()
         initPopular()
         initTopRated()
-
-        nowPlayingMore.setOnClickListener {
-            requireActivity().supportFragmentManager.beginTransaction().apply {
-                add(R.id.fragmentContainer, MoviePageFragment::class.java, null, TAG)
-                addToBackStack(null)
-                commit()
-            }
-        }
     }
 
     private fun initNowPlaying() {
         val nowPlayingAdapter = MoviesAdapter()
         moviesViewModel.nowPlayingList.observe(viewLifecycleOwner) {
-            nowPlayingTitle.visibility = if (it.isLoading) View.GONE else View.VISIBLE
+            nowPlayingList.title.visibility = if (it.isLoading) View.GONE else View.VISIBLE
             nowPlayingAdapter.submitList(it.data.toMutableList())
         }
 
-        nowPlayingList.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        nowPlayingList.adapter = nowPlayingAdapter
+        nowPlayingList.list.adapter = nowPlayingAdapter
+        nowPlayingList.onEventListener = object : SimpleCategoryList.OnEventListener {
+            override fun onViewAll() {
+                toViewAll(MovieCategory.NowPlaying)
+            }
+        }
     }
 
     private fun initPopular() {
         val popularAdapter = MoviesAdapter()
         moviesViewModel.popularList.observe(viewLifecycleOwner) {
-            popularTitle.visibility = if (it.isLoading) View.GONE else View.VISIBLE
+            popularList.title.visibility = if (it.isLoading) View.GONE else View.VISIBLE
             popularAdapter.submitList(it.data.toMutableList())
         }
 
-        popularList.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        popularList.adapter = popularAdapter
+        popularList.list.adapter = popularAdapter
+        popularList.onEventListener = object : SimpleCategoryList.OnEventListener {
+            override fun onViewAll() {
+                toViewAll(MovieCategory.Popular)
+            }
+        }
     }
 
     private fun initTopRated() {
         val topRatedAdapter = MoviesAdapter()
         moviesViewModel.topRatedList.observe(viewLifecycleOwner) {
-            topRatedTitle.visibility = if (it.isLoading) View.GONE else View.VISIBLE
+            topRatedList.title.visibility = if (it.isLoading) View.GONE else View.VISIBLE
             topRatedAdapter.submitList(it.data.toMutableList())
         }
 
-        topRatedList.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        topRatedList.adapter = topRatedAdapter
+        topRatedList.list.adapter = topRatedAdapter
+        topRatedList.onEventListener = object : SimpleCategoryList.OnEventListener {
+            override fun onViewAll() {
+                toViewAll(MovieCategory.TopRated)
+            }
+        }
+    }
+
+    private fun toViewAll(category: MovieCategory) {
+        // TODO: show fragment for the category
+        requireActivity().supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragmentContainer, MoviePageFragment::class.java, null, TAG)
+            addToBackStack(null)
+            commit()
+        }
     }
 }
--- app/src/main/java/com/flowerhop/movielibrary/view/SimpleCategoryList.kt
+++ app/src/main/java/com/flowerhop/movielibrary/view/SimpleCategoryList.kt
@@ -7,6 +7,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.flowerhop.movielibrary.databinding.SimpleCategoryListBinding
+import kotlinx.android.synthetic.main.simple_category_list.view.*
 
 class SimpleCategoryList @JvmOverloads constructor(
     context: Context,
@@ -15,31 +16,17 @@ class SimpleCategoryList @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
     interface OnEventListener {
         fun onViewAll()
-        fun onScrolledPosition(position: Int)
     }
 
     private val binding: SimpleCategoryListBinding
-    private var onEventListener: OnEventListener? = null
+    var onEventListener: OnEventListener? = null
 
     init {
         val inflater = LayoutInflater.from(context)
         binding = SimpleCategoryListBinding.inflate(inflater, this).apply {
-            nowPlayingMore.setOnClickListener { onEventListener?.onViewAll() }
+            more.setOnClickListener { onEventListener?.onViewAll() }
             val linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-            nowPlayingList.layoutManager = linearLayoutManager
-            nowPlayingList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                    super.onScrollStateChanged(recyclerView, newState)
-                    if (newState != RecyclerView.SCROLL_STATE_IDLE) return
-                    val currentItemPosition = linearLayoutManager.findLastVisibleItemPosition()
-                    onEventListener?.onScrolledPosition(currentItemPosition)
-                }
-            })
+            list.layoutManager = linearLayoutManager
         }
-    }
-
-    fun setAdapter(adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) {
-        binding.nowPlayingList.adapter = adapter
-        binding.nowPlayingList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
     }
 }
